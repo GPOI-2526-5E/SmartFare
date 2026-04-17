0e552a57a28daae01db028083e433fadb3f3682d
nico3228 <n.pansardi.3228@vallauri.edu>
2026-04-17 09:51:02 +0200

    feat: add experiences-carousel component to home feature

---
+++ Smartfare-Frontend/src/app/features/home/experiences-carousel/experiences-carousel.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-experiences-carousel',
+  imports: [],
+  templateUrl: './experiences-carousel.html',
+  styleUrl: './experiences-carousel.css',
+})
+export class ExperiencesCarousel {
+
+}
