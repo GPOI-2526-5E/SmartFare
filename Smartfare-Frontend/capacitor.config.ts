702ea83843ed025a54aa42ee9855442c751b8589
IgorTsaturyan <i.tsaturyan.3329@vallauri.edu>
2026-06-03 22:22:38 +0200

    andorid app

---
+++ Smartfare-Frontend/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.smartfare.app',
+  appName: 'Smartfare-app',
+  webDir: 'dist/SmartFare/browser'
+};
+
+export default config;
--- Smartfare-Frontend/src/environments/environment.ts
+++ Smartfare-Frontend/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3224',
+  apiUrl: 'http://localhost:2345',
   siteUrl: 'http://localhost:4200'
 };
